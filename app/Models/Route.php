09aab969350c74793aa6da88f477be48f62350c0
Hamza Maach <hamzamaach56@gmail.com>
2024-07-31 18:13:15 +0100

    format the time to be 'H:i'

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Route extends Model
 {
     use HasFactory;
+
+    protected $fillable = [];
+
+    public function passengers()
+    {
+        return $this->hasMany(Passenger::class);
+    }
+
+    public function getTimeAttribute($value)
+    {
+        return \Carbon\Carbon::createFromFormat('H:i:s', $value)->format('H:i');
+    }
 }
