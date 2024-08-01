9d96dad31093de7dd7342f6ee8b935e60c20faab
Hamza Maach <hamzamaach56@gmail.com>
2024-08-01 18:19:23 +0100

    create model for allowed passengers

---
+++ database/seeders/AllowedPassengersSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AllowedPassengersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
