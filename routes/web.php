13f090b5463cf5aab1f984d089220df10d513d6b
Hamza Maach <hamzamaach56@gmail.com>
2024-08-01 18:25:04 +0100

    create route for storing new passengers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PassengerController;
 use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [RouteController::class, 'index']);
+Route::post('/routes/{route}/passengers', [PassengerController::class, 'store']);
