bd50dadcea67d56413e8ba94546da4e529372dbc
Hamza Maach <hamzamaach56@gmail.com>
2024-08-01 18:17:56 +0100

    create the navbar

---
+++ resources/views/index/navbar.blade.php
@@ -0,0 +1,17 @@
+<div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+            <h4 class="text-white">Collapsed content</h4>
+            <span class="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a href="https://zone01oujda.ma/" class="zone-logo">
+            <img src="{{ asset('images/zone_logo.png') }}" alt="Zone01 Oujda">
+        </a>
+    </nav>
+</div>
