7d29782970cefb91349902ec14da7cf567041aca
Hamza Maach <hamzamaach56@gmail.com>
2024-08-08 15:52:10 +0100

    create index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class=" text-gray-900">
+                @include('layouts.routes')
+            </div>
+        </div>
+    </div>
+</x-app-layout>
