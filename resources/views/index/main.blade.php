611677abda0ddca5f5f92d5e71b655f1384e6418
Hamza Maach <hamzamaach56@gmail.com>
2024-08-08 10:32:46 +0100

    Added Breeze and updated configuration for Heroku deployment

--- resources/views/index/main.blade.php
+++ resources/views/index/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
     {{-- <link rel="stylesheet" href="https://bus-management-4eeaca6cf3ad.herokuapp.com/css/app.css"> --}}
-    <link rel="stylesheet" href="http://bus-management-4eeaca6cf3ad.herokuapp.com/build/assets/app-Bx8TtvEs.css">
+    <link rel="stylesheet" href="https://bus-management-4eeaca6cf3ad.herokuapp.com/build/assets/app-Bx8TtvEs.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
@@ -19,7 +19,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
