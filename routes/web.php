b7cbd590cf9562f3fe76f977619f597a2ba528de
Hamza Maach <hamzamaach56@gmail.com>
2024-08-08 15:49:42 +0100

    add necessary routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PassengerController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RouteController;
@@ -12,14 +13,20 @@ Route::get('/welcome', function () {
 Route::get('/', [RouteController::class, 'index'])->name('home');
 Route::post('/routes/{route}/passengers', [PassengerController::class, 'store']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    // function () {
+//     return view('dashboard');
+// })
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/routes/{route}/edit', [RouteController::class, 'edit'])->name('routes.edit');
+    Route::get('/routes/{route}', [RouteController::class, 'show'])->name('routes.show');
+    Route::put('/routes/{route}', [RouteController::class, 'update'])->name('routes.update');
+    Route::put('/routes/{route}', [RouteController::class, 'destroy'])->name('routes.destroy');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
