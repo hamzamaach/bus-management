6d893c6e783929816a11a65b96e7bd4299f96e9c
Hamza Maach <hamzamaach56@gmail.com>
2024-08-08 10:40:07 +0100

    Force HTTPS and update URL generation

--- resources/views/index/main.blade.php
+++ resources/views/index/main.blade.php
@@ -4,9 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
-    {{-- <link rel="stylesheet" href="https://bus-management-4eeaca6cf3ad.herokuapp.com/css/app.css"> --}}
-    <link rel="stylesheet" href="https://bus-management-4eeaca6cf3ad.herokuapp.com/build/assets/app-Bx8TtvEs.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
